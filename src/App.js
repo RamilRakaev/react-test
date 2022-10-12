ebbbb4697eef03bd3d2e66fd58ed962be506845d
Рамиль Ракаев <ramrg546@gmail.com>
2022-10-12 20:52:29 +0300

    Counter and post components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+﻿import ClassCounter from './components/ClassCounter';
+import Counter from './components/Counter';
+import './styles/App.css';
+import React, { useState } from 'react';
+import PostList from './components/PostList';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    const [posts, setPosts] = useState([
+        { id: 1, title: 'JavaScript', body: 'Описание' },
+        { id: 2, title: 'CSS', body: 'Описание' },
+        { id: 3, title: 'HTML', body: 'Описание' },
+    ])
+    return (
+        <div className="App">
+            <Counter />
+            <ClassCounter />
+            <PostList posts={posts} title="Список постов" />
+        </div>
+    );
 }
 
 export default App;
