0b6f5abc312808207111f45ef02abfb5fcb72c18
RamilRakaev <59777361+RamilRakaev@users.noreply.github.com>
2022-10-18 23:15:31 +0300

    Added routes. Fixed error in AppRouter because routes

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from '../components/UI/Navbar/Navbar';
-import About from '../pages/About';
 import Posts from '../pages/Posts';
-import Error from '../pages/Error';
+import { routes } from '../router';
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <Navbar />
             <Routes>
-                <Route path="/about" element={<About />} />
-                <Route path="/posts" element={<Posts />} />
-                <Route path="/error" element={<Error />} />
-                <Route path="*" element={<Error />} />
+                { routes.map(route => 
+                    <Route
+                        key={route.path}
+                        exact={route.exact}
+                        path={route.path}
+                        element={route.component}
+                    />
+                )}
+                <Route path="*" elementk={<Posts />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ src/pages/PostIdPage.jsx
@@ -0,0 +1,48 @@
+﻿import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import PostService from '../API/PostService';
+import Loader from '../components/UI/Loader/Loader';
+import { useFetching } from '../hooks/useFetching';
+
+const PostIdPage = () => {
+    const params = useParams();
+    const [post, setPost] = useState({});
+    const [comments, setComments] = useState([]);
+    const [fetchPostById, isLoading, error] = useFetching(async (id) => {
+        const response = await PostService.getById(id);
+        setPost(response.data)
+    });
+    const [fetchComments, commentsIsLoading, commentsError] = useFetching(async (id) => {
+        const response = await PostService.getCommentsByPostId(id);
+        setComments(response.data)
+    });
+
+    useEffect(() => {
+        fetchPostById(params.id);
+        fetchComments(params.id);
+    }, []);
+
+    return (
+        <div>
+            <h1>ID: {params.id}</h1>
+            {isLoading
+                ? <Loader />
+                : <div>{post.id}. {post.title}</div>
+            }
+            <h1>Комментарии</h1>
+            {commentsIsLoading
+                ? <Loader />
+                : <div>
+                    {comments.map(comment =>
+                        <div key={comment.id} style={{ marginTop: 15 }}>
+                            <h5>{comment.email}</h5>
+                            <div>{comment.body}</div>
+                        </div>
+                    )}
+                </div>
+            }
+        </div>
+    );
+};
+
+export default PostIdPage;
