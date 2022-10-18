0d49117fed38fd2fd87bf5bc89f4d4661ba180a8
RamilRakaev <59777361+RamilRakaev@users.noreply.github.com>
2022-10-19 01:57:32 +0300

    endless tape

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-﻿import React, { useEffect, useState } from 'react';
+﻿import React, { useEffect, useState, useRef } from 'react';
 import PostService from '../API/PostService';
 import ClassCounter from '../components/ClassCounter';
 import Counter from '../components/Counter';
@@ -10,9 +10,12 @@ import MyButton from '../components/UI/button/MyButton';
 import Loader from '../components/UI/Loader/Loader';
 import MyModal from '../components/UI/MyModal/MyModal';
 import { useFetching } from '../hooks/useFetching';
+import { userObserver } from '../hooks/useObserver';
 import { usePosts } from '../hooks/usePosts';
 import '../styles/App.css';
 import { getPageCount } from '../utils/pages';
+import { useObserver } from '../hooks/useObserver';
+import MySelect from '../components/UI/select/MySelect';
 
 function Posts() {
     const [posts, setPosts] = useState([]);
@@ -22,17 +25,22 @@ function Posts() {
     const [limit, setLimit] = useState(10);
     const [page, setPage] = useState(1);
     const sortedSearchedPosts = usePosts(posts, filter.sort, filter.query);
+    const lastElement = useRef();
 
     const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
         const response = await PostService.getAll(limit, page);
-        setPosts(response.data);
+        setPosts([...posts, ...response.data]);
         const totalCount = response.headers['x-total-count'];
         setTotalPages(getPageCount(totalCount, limit));
     })
 
+    useObserver(lastElement, page < totalPages, isPostsLoading, () => {
+        setPage(page + 1);
+    });
+
     useEffect(() => {
         fetchPosts(limit, page);
-    }, []);
+    }, [page, limit]);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
@@ -45,13 +53,13 @@ function Posts() {
 
     const changePage = (page) => {
         setPage(page);
-        fetchPosts(limit, page);
     };
 
     return (
         <div className="App">
             <Counter init={5} />
             <ClassCounter />
+
             <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
                 Создать элемент
             </MyButton>
@@ -63,15 +71,30 @@ function Posts() {
                 filter={filter}
                 setFilter={setFilter}
             />
+            <MySelect
+                value={limit}
+                onChange={value => setLimit(value)}
+                defaultValue='Объем страницы'
+                options={[
+                    { value: 5, name: '5' },
+                    { value: 10, name: '10' },
+                    { value: 15, name: '15' },
+                    { value: 20, name: '20' },
+                    { value: -1, name: 'Все посты' },
+                ]}
+            />
+
             {postError &&
                 <h1>Ошибка: {postError}</h1>
             }
-            {isPostsLoading
-                ? <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}>
+
+            <PostList remove={removePost} posts={sortedSearchedPosts} title='Список постов' />
+            <div ref={lastElement}></div>
+            {isPostsLoading &&
+                <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}>
                     <Loader />
-                </div>
-                : <PostList remove={removePost} posts={sortedSearchedPosts} title='Список постов' />
-            }
+                </div>}
+
             <Pagitaion
                 totalPages={totalPages}
                 page={page}
